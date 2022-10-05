94b23d5c0fd1350c829df51fe161cbe6a9ecb355
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-05 21:50:59 +0530

    Calculating DailyWage of Employee

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,4 +1,7 @@
 ﻿using EmployeeWage;
 
-UC1_EmployeeCheck ran = new UC1_EmployeeCheck();
-ran.Employee();
+//UC1_EmployeeCheck ran = new UC1_EmployeeCheck();
+//ran.Employee();
+
+UC2_Check_DailyWage ran1 = new UC2_Check_DailyWage();
+ran1.Employee1();
